82856410d264433424c4f622c7e1c819f6f70627
Emillio <emilliokanz@gmail.com>
2023-02-14 17:47:38 +0700

    update user returns user data

--- packages/rest-api/src/users/users.service.ts
+++ packages/rest-api/src/users/users.service.ts
@@ -99,7 +99,7 @@ export class UsersService {
         );
         return;
       }
-      await this.prisma.user.update({
+      const data = await this.prisma.user.update({
         where: {
           id: userId,
         },
@@ -119,6 +119,7 @@ export class UsersService {
       return {
         status: HttpStatus.OK,
         message: 'User has been updated',
+        data,
       };
     } catch (e) {
       this.logger.log(e.message);
