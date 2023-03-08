cf236d780e1011e0573a063a11e63a0731a2b47c
Emillio <emilliokanz@gmail.com>
2023-03-08 13:11:14 +0700

    listingid in notification into nullable

---
+++ packages/database/migrations/20230308061001_listingid_in_notification_into_nullable/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "NotificationDetailBid" ALTER COLUMN "listingId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "NotificationDetailSale" ALTER COLUMN "listingId" DROP NOT NULL;
